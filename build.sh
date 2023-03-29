6aa19459496887ba0c68c4af994d1ce4db9634a2
kashvigarg <kashvigarg02@gmail.com>
2023-03-29 08:51:06 +0530

    added waitress

--- build.sh
+++
@@ -1,6 +0,0 @@
-set -o errexit  # exit on error
-
-pip install -r requirements.txt
-
-python ./kavach/manage.py collectstatic --no-input
-python ./kavach/manage.py migrate
--- kavach/kavach/settings.py
+++ kavach/kavach/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -118,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_STORAGFE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
